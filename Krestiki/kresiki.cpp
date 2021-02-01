0d1546ee112d3faa611e68eb82e7094affed690a
Klopokayzer <liber150395@gmail.com>
2021-02-01 19:06:18 +0300

    Создано начало для игры в крестики-нолики (2-4 задание)

---
+++ Krestiki/kresiki.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main ()
+{
+	enum symbol{pusto, O, X}; // задание 2
+	int pole[3][3]={{pusto, pusto, pusto},{pusto, pusto, pusto},{pusto, pusto, pusto}}; // задание 3
+	
+	//4 задание
+	struct igra {
+	char name [15];
+	int schet;
+	} igrok;
+	return 0;
+}
